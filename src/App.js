cfdcef9b7caf735151bfda12cb33ea1e4f2256d2
Felix <moin@fawagner.de>
2020-08-05 23:27:05 +0200

    Add fonts and header, also use scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            React deployed using CapRover!
-          </p>
-          <a
-            className="App-link"
-            href="https://caprover.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CapRover.com
-          </a>
+          <h1 className="App-title">
+            <span className="App-title__main">Felix Wagner</span>
+            <span className="App-title__subtitle">PORTFOLIO</span>
+          </h1>
         </header>
       </div>
     );
